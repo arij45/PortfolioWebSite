50d0725c8f8e323b7adf528fe81305792ad7ac92
Arij kortas <arij.kortas@enetcom.u-sfax.tn>
2025-02-04 15:31:22 +0100

    new commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -31,7 +31,7 @@ const Navbar = () => {
         <li><AnchorLink className='anchor-link'offset={50} href='#work'><p onClick={()=>setMenu("work")}>Portfolio</p></AnchorLink>{menu=="work"?<img src={themePatternImage} alt=''/>:<></>}</li>
         <li><AnchorLink className='anchor-link'offset={50} href='#contact'><p onClick={()=>setMenu("contact")}>Contact</p></AnchorLink>{menu=="contact"?<img src={themePatternImage} alt=''/>:<></>}</li>
         </ul>
-        <div><AnchorLink className='nav-connect' href='#contact'>Connect With Mee</AnchorLink></div>
+        <div><AnchorLink className='nav-connect' href='#contact'>Connect With Me</AnchorLink></div>
     </div>
   )
 }
